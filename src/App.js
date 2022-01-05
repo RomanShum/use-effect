2ca9c56c910caab4e5e6492c106575090ac1eab6
Roman <Romch1k1@yandex.ru>
2022-01-05 15:46:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import List from "./components/List";
+import Details from "./components/Details";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    firstLoad();
+  }, []);
+
+  const firstLoad = () => {
+    fetch(
+      "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json"
+    )
+      .then((resp) => resp.json())
+      .then(function (data) {
+        setUsers(data);
+      });
+  };
+
+  const onHandleClick = (id, name) => {
+    setInfo({ id: id, name: name });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <List onHandleClick={onHandleClick} users={users} />
+      <Details info={info} />
+    </>
   );
 }
 
---
+++ src/components/Details.js
@@ -0,0 +1,57 @@
+import { useEffect, useState, useRef } from "react";
+
+function Details(props) {
+  const [detailsData, setDetailsData] = useState({});
+  const [id, setId] = useState(0);
+  const [load, setLoad] = useState(false);
+
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    setId(props.info.id);
+  }, []);
+
+  useEffect(() => {
+    const new_id = props.info.id;
+    if (new_id !== intervalRef.current) {
+      setId(new_id);
+      intervalRef.current = new_id;
+      setLoad(true);
+      detailsLoad(new_id);
+    }
+    return setDetailsData({});
+  }, [props.info]);
+
+  const detailsLoad = () => {
+    fetch(
+      `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${props.info.id}.json`
+    )
+      .then((resp) => resp.json())
+      .then(function (data) {
+        setDetailsData(data);
+        setLoad(false);
+      });
+  };
+
+  if (load) {
+    return "Загрузка";
+  }
+
+  return (
+    <div>
+      {detailsData.id !== undefined ? (
+        <>
+          <img src={detailsData.avatar} alt={detailsData.name} />
+          <p>{detailsData.name}</p>
+          <p>City: {detailsData.details.city}</p>
+          <p>Company: {detailsData.details.company}</p>
+          <p>Position: {detailsData.details.position}</p>
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default Details;
